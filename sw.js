0f5e83a781108b4a3c24059e78d8ac158098c09f
kumarsonu1234882-blip <kumarsonu1234882-blip@users.noreply.github.com>
2026-09-21 18:47:03 +0530

    Fix old StudyVault service worker cache

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-const CACHE='studyvault-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./data/settings.json','./data/subjects.json','./data/resources.json','./data/timetable.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
+const CACHE = "studyvault-v9-fresh";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
